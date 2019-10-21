2b96e93f519534f4e3d906d7f2441c6bd4046b49
BrandonRTL <43331560+BrandonRTL@users.noreply.github.com>
2019-10-22 00:52:29 +0300

    Inceprion

--- include/UStack.h
+++ include/UStack.h
@@ -1,6 +1,8 @@
 ﻿#ifndef __TSTACK_H__
 #define __TSTACK_H__
-
+#include <string>
+#include <vector>
+#include <math.h>
 const int MAX_STACK_SIZE = 100000000;
 
 template <class ValType>
@@ -104,4 +106,138 @@ public:
 		return MinStack.Top();
 	}
 };
+int Prioritet(char s)
+{
+	switch (s)
+	{
+	case '+': return 2;
+	case '-': return 2;
+	case '*': return 3;
+	case '/': return 3;
+	case '(': return 1;
+	case ')': return 0;
+	default:
+		return -1;
+	}
+}
+
+std::string OPZPerevod(std::string _A) // пока что только цифры давайте напишем (3+2)
+{
+	TStack<char> OPZ;
+	int SK = 0;
+	char tmp;
+	std::string OPZ2;
+	for (int i = 0; i < _A.size(); i++)
+	{
+		//std::cout << "TOP " << OPZ.Top() << std::endl;
+	//	std::cout << "STRING " << OPZ2 << std::endl;
+		if (_A[i] == '(')
+		{
+			OPZ.Push(_A[i]);
+			std::cout << "WHILE V SKOBKE " << OPZ.Top() << std::endl;
+		}
+		if (_A[i] == ')')
+		{
+			while (OPZ.Top() != '(')
+			{
+				tmp = OPZ.Pop();
+				std::cout << "WHILE V SKOBKE " << OPZ.Top() << std::endl; //(2+2)
+				OPZ2 = OPZ2 + tmp;
+			}
+			OPZ.Pop();
+		}
+		if (_A[i] >= 48 && _A[i] <= 59)
+		{
+			OPZ2 = OPZ2 + _A[i];
+			if (_A[i+1] < 48 || _A[i+1] > 59)
+			{
+				OPZ2 = OPZ2 + '|';
+			}
+
+		}
+		if (_A[i] == '+' || _A[i] == '-' || _A[i] == 'e' || _A[i] == '^' || _A[i] == '/' || _A[i] == '*')
+		{
+			if (OPZ.IsEmpty())
+			{
+				OPZ.Push(_A[i]);
+			}
+			else
+			{
+				if (Prioritet(_A[i]) > Prioritet(OPZ.Top()))
+				{
+					OPZ.Push(_A[i]);
+				}
+				else 
+				{
+					while (!OPZ.IsEmpty() && (Prioritet(OPZ.Top() >= Prioritet(_A[i]))))
+					{
+						OPZ2 = OPZ2 + OPZ.Pop();
+					}
+					OPZ.Push(_A[i]);
+				}
+			}
+		}
+	}
+	while (!OPZ.IsEmpty())
+	{
+		OPZ2 = OPZ2 + OPZ.Pop();
+	}
+	return OPZ2;
+}
+double OPZSolvev2(std::string _A) 
+{
+	double tmp;
+	TStack<double> OPZ;
+	int count = 0;
+	for (int i = 0; i < _A.size(); i++)
+	{
+		std::cout << "TOP " << OPZ.Top() << std::endl;
+		switch (_A[i])
+		{
+		case '+':
+			OPZ.Push((OPZ.Pop()) + (OPZ.Pop()));
+
+			break;
+		case '-':
+			tmp = OPZ.Pop();
+			OPZ.Push((OPZ.Pop()) - tmp);
+			break;
+		case '*':
+			OPZ.Push((OPZ.Pop())*(OPZ.Pop()));
+			break;
+		case '/':
+			tmp = OPZ.Pop();
+			OPZ.Push((OPZ.Pop()) / tmp);
+			break;
+		case 'e':
+			OPZ.Push(exp(OPZ.Pop()));
+			break;
+		case 's':
+			OPZ.Push(sin(OPZ.Pop()));
+			break;
+		case 'c':
+			OPZ.Push(cos(OPZ.Pop()));
+			break;
+		case '^':
+			OPZ.Push(pow(OPZ.Pop(), OPZ.Pop()));
+			break;
+		case '|':
+			break;
+		default:
+			std::cout << "COUNT - cifri f 4isle " << count << std::endl;
+			if (count == 0)
+				tmp = _A[i]-48;
+			else tmp = tmp * 10 + _A[i]-48;
+			if (_A[i + 1] < 48 || _A[i + 1] > 59)
+			{
+				OPZ.Push(tmp);
+				count = -1;
+				tmp = 0;
+			}
+			count++;
+		}
+	}
+	return OPZ.Top();
+}
+
 #endif
--- samples/sample_stack.cpp
+++ samples/sample_stack.cpp
@@ -4,12 +4,18 @@
 using namespace std;
 int main()
 {
-	TMinStack<int> A;
+	/*TMinStack<int> A;
 	A.Push(2);
 	A.Push(10);
 	A.Push(1);
 	A.Push(-10);
 	int x = A.FindMin();
+	cout << x; */
+	double x;
+	string A = "(3+2*(8-12))";
+	string B = OPZPerevod(A);
+	cout << B;
+	x = OPZSolvev2(B);
 	cout << x;
 	cin >> x;
 	return 0;
